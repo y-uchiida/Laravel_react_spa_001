12c75e7beeb4abc0110682b702ebfac250feb17f
yugo uchiida <y.uchiida@nirneege.com>
2023-01-08 22:12:41 +0900

    Task のマイグレーションファイルを設定

--- database/migrations/2023_01_08_220801_create_tasks_table.php
+++ database/migrations/2023_01_08_220801_create_tasks_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
     }
