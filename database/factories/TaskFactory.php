39d648aaaa9e55a91876544cf9741f13b17b339a
yugo uchiida <y.uchiida@nirneege.com>
2023-01-08 22:14:48 +0900

    TaskFactory を設定

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -17,7 +17,10 @@ class TaskFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->realText(rand(15, 40)),
+            'is_done' => $this->faker->boolean(10),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
