fce34638722ab60eedceb7435c77e0d177a60e22
yugo uchiida <y.uchiida@nirneege.com>
2023-01-09 07:37:15 +0900

    Task ページにタスクの一覧を表示する

--- resources/js/pages/tasks/components/TaskPage.tsx
+++ resources/js/pages/tasks/components/TaskPage.tsx
@@ -1,5 +1,23 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+import { Task } from "../../../types/Task";
+
+
 export const TaskPage = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    const getTasks = async () => {
+        const { data } = await axios.get<Task[]>('api/tasks');
+        setTasks(data);
+    }
+
     return (
-        <div>TaskPage</div>
+        tasks.map((task) => {
+            return <div key={task.id}>{task.title}</div>
+        })
     )
 }
