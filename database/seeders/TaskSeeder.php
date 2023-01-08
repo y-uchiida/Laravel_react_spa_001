5eade6528c79b5ff2fea1ae1d1f36586904c584a
yugo uchiida <y.uchiida@nirneege.com>
2023-01-08 22:17:21 +0900

    TaskSeeder の設定

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Task;
 
 class TaskSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Task::factory()->count(10)->create();
     }
 }
