5fe1537d68253dc984b1692bffebf622abddfa6b
yugo uchiida <y.uchiida@nirneege.com>
2023-01-11 02:11:14 +0900

    React から updateDone を呼び出す処理を実装

--- resources/js/queries/TaskQuery.ts
+++ resources/js/queries/TaskQuery.ts
@@ -1,7 +1,19 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient, useMutation } from "react-query";
 
-import { getTasks } from "../api/TaskApi";
+import { getTasks, updateDoneTask } from "../api/TaskApi";
 
 export const useTasks = () => {
     return useQuery('tasks', () => getTasks());
+}
+
+export const useUpdateDoneTask = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation(updateDoneTask, {
+        // updateDoneTask を実行し、成功したらonSuccess が呼び出される
+        // invalidateQueries でキャッシュを破棄してデータを再取得する
+        onSuccess: () => {
+            queryClient.invalidateQueries('tasks');
+        }
+    });
 }
