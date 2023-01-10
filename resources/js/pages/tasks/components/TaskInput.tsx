a502e231b8152dfb13a4b33a906b1402220f9985
yugo uchiida <y.uchiida@nirneege.com>
2023-01-11 03:23:10 +0900

    画面からtask を追加できるように実装

--- resources/js/pages/tasks/components/TaskInput.tsx
+++ resources/js/pages/tasks/components/TaskInput.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import { useCreateTask } from "../../../queries/TaskQuery";
+
 export const TaskInput = () => {
+    const createTask = useCreateTask();
+    const [title, setTitle] = useState('');
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        // 実行後にtasks の値を変更させるので、mutate()をよびだす
+        createTask.mutate(title);
+        setTitle('');
+    }
+
     return (
         <form>
             <div className='inner'>
-                <input type="text" className='' placeholder='タスク名を入力してください' defaultValue='' />
-                <button className='btn is-primary'>追加</button>
+                <input
+                    type="text"
+                    className=''
+                    placeholder='タスク名を入力してください'
+                    onChange={(e) => { setTitle(e.target.value) }}
+                    value={title}
+                />
+                <button
+                    type="button"
+                    className='btn is-primary'
+                    onClick={handleClick}
+                >追加</button>
             </div>
         </form>
     )
