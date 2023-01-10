b54a81b8e8388493a2e0b21cd1dbc0cf4e1ed1b9
yugo uchiida <y.uchiida@nirneege.com>
2023-01-11 06:31:29 +0900

    ログイン、ログアウトを行うためのAPIエンドポイントを作成

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout']);
 
 // sanctum で認証されているユーザーのみがアクセスできるようにミドルウェアを通す
 Route::group(['middleware' => 'auth:sanctum'], function () {
