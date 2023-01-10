6f7fd46e80b6a5509a2b000cda507e3c7ad5a5f7
yugo uchiida <y.uchiida@nirneege.com>
2023-01-11 02:21:58 +0900

    is_done がtrue のtask はタイトルに打消し線を表示する

--- resources/js/pages/tasks/components/TaskItem.tsx
+++ resources/js/pages/tasks/components/TaskItem.tsx
@@ -1,17 +1,27 @@
 import { Task } from '../../../types/Task'
+import { useUpdateDoneTask } from '../../../queries/TaskQuery';
 
 type Props = {
     task: Task
 }
 
 export const TaskItem = ({ task }: Props) => {
+    const updateDoneTask = useUpdateDoneTask();
+
     return (
         <div>
             <li className={task.is_done ? 'done' : ''}>
                 <label className="checkbox-label" htmlFor="">
-                    <input type="checkbox" className="checkbox-input" checked={task.is_done} />
+                    <input
+                        type="checkbox"
+                        className="checkbox-input"
+                        checked={task.is_done}
+                        onClick={() => updateDoneTask.mutate(task)}
+                    />
                 </label>
-                <span>
+                <span style={{
+                    textDecoration: task.is_done ? 'line-through' : undefined,
+                }}>
                     {task.title}
                 </span>
             </li>
